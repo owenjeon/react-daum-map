41377f5ea5a072f1dd7432442925409fa55a1d29
owen <owen.jeon@gmail.com>
2018-07-31 16:03:08 +0900

    add control

--- src/modules/Control.tsx
+++ src/modules/Control.tsx
@@ -1,7 +1,11 @@
 import React from "react"
 import {ControlPosition, zoomControl} from "../controls"
+import {inject} from "mobx-react"
 
-export class Control extends React.Component<Props>{
+@inject('mapStore')
+export class Control extends React.Component<Props & {mapStore: {map:any}}>{
+
+    private controls;
     componentDidMount(){
         console.log('componentDidMount')
     }
@@ -11,8 +15,24 @@ export class Control extends React.Component<Props>{
     render(){
         return null;
     }
+
+    setControl(){
+        this.props.controls.forEach(v => {
+            this.controls.set(v.control, Object.assign({}, v, {status:this.controls.has(v.control) ? 'override': 'new'}))
+        })
+        this.controls.forEach((v, k)=>{
+            if(v.status === 'old'){
+                this.controls.delete(k);
+                this.props.mapStore.map.removeControl(k, v.position);
+                return;
+            }
+            if(v.status === 'new') this.props.mapStore.map.addControl(k, v.position);
+            v.status = 'old';
+        })
+    }
 }
 
 interface Props {
-    controls: {control: any, position: any, status?: 'new' | 'old' | 'override'}[]
+    controls: {control: any, position: any, status?: 'new' | 'old' | 'override'}[],
+
 }
